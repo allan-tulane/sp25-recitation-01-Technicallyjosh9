20f6e062da8a8613cb80eb04a33f8127f16d50fd
Joshua Haddad <86764799+Technicallyjosh9@users.noreply.github.com>
2025-01-30 03:31:19 +0000

    Joshua Haddad Lab #1

--- test_main.py
+++ test_main.py
@@ -13,10 +13,13 @@ def test_binary_search():
 	assert binary_search([1, 2, 3, 4, 5], 6) == -1
 
 	### TODO: add two more tests here.
-	assert binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],3) == 2
-	assert binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],5) == 4
+	assert binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+	                     3) == 2
+	assert binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+	                     5) == 4
 
-	### 
+
+	###
 def test_compare_search():
 	res = compare_search(sizes=[10, 100])
 	print(res)
